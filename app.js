9fecad000e7c40ed14e0c0b81552b1b426e2283b
Janson Maharaj <40618018+91030362@users.noreply.github.com>
2019-07-04 10:41:38 +1200

    added comment section

--- app.js
+++ app.js
@@ -2,36 +2,16 @@ var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
+var Campground = require("./models/campgrounds");
+var seedDB = require("./seeds");
+var Comment = require("./models/comments");
 
+seedDB();
 mongoose.connect("mongodb://localhost:27017/yelp_camp", {useNewUrlParser: true});
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 
-// SCHEMA SETUP
-var campgroundSchema = new mongoose.Schema({
-    name: String,
-    image: String,
-    description: String
-});
-
-var Campground = mongoose.model("Campground", campgroundSchema);
-
-// save campground to db
-// Campground.create({
-//         name: "auckland camp",
-//         image: " https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVHHZ98u6Sj1KavFhu7XPQeH8l1G2SqHn0wl7rvGIdielR7ZmC",
-//         description: "beautiful campground by the sea"
-//     },
-//     function (err, campground) {
-//         if (err) {
-//             console.log("some error was made when creating a new campground");
-//             console.log(err);
-//         } else {
-//             console.log("campground has benn saved to db");
-//             console.log(campground);
-//         }
-//     })
 
 
 app.get("/", function (req, res) {
@@ -39,15 +19,15 @@ app.get("/", function (req, res) {
     // res.send("landing page");
 });
 
+//CAMPGROUNDS - shows all campgrounds
 app.get("/campgrounds", function (req, res) {
     //GET ALL CAMPGROUNDS FROM DB
     Campground.find({}, function (err, allCampgrounds) {
         if (err) {
-            console.log("some error was made when looking for campgrounds in db");
             console.log(err);
 
         } else {
-            res.render("campgrounds.ejs", {
+            res.render("campgrounds/campgrounds.ejs", {
                 campgrounds: allCampgrounds
             });
 
@@ -60,11 +40,15 @@ app.get("/campgrounds", function (req, res) {
 app.post("/campgrounds", function (req, res) {
     var name = req.body.name;
     var image = req.body.image;
+    var imageCity = req.body.imageCity;
+    var descriptionCity = req.body.descriptionCity;
     var description = req.body.description;
     var newCampground = {
         name: name,
         image: image,
-        description:description
+        imageCity: imageCity,
+        description:description,
+        descriptionCity:descriptionCity
     }
     //CREATE A NEW CAMPGROUND AND SAVE TO DB
     Campground.create(newCampground, function (err, newlyCreated) {
@@ -79,26 +63,66 @@ app.post("/campgrounds", function (req, res) {
 
 
 app.get("/campgrounds/new", function (req, res) {
-    res.render("new.ejs");
+    res.render("campgrounds/new.ejs");
 });
 
 //SHOW- shows more info about campground
 app.get("/campgrounds/:id" , function(req,res) {
     //find the campground with provide Id
-    Campground.findById(req.params.id, function(err, foundCampground) {
+    Campground.findById(req.params.id).populate("comments").exec(function(err, foundCampground) {
         if(err) {
             console.log(err);
 
         } else {
+            console.log(foundCampground);
             //render show template with that campground
-            res.render("show.ejs", {campground: foundCampground});
+            res.render("campgrounds/show.ejs" , {campground: foundCampground});
 
         }
 
+    });
+});
+
+// ===========================
+// Comments route
+// ===========================
+app.get("/campgrounds/:id/comments/new" , function(req,res) {
+    //find campground by id
+    Campground.findById(req.params.id, function(err, campground) {
+        if(err){
+            console.log(err);
+        } else {
+            res.render("comments/new.ejs", {campground: campground});
+
+        }
     })
 });
 
+app.post("/campgrounds/:id/comments", function (req,res) {
+    // lookup campground using ID
+    Campground.findById(req.params.id, function(err, campground) {
+        if(err) {
+            console.log(err);
+            res.redirect("/campgrounds");
+        }else {
+           
+            //create new comment
+            Comment.create(req.body.comment, function (err, comment) {
+                if(err) {
+                    console.log(err);
+                } else {
+                    //connect new comment to campground
+                    campground.comments.push(comment);
+                    campground.save();
+                    //redirect campground show page
+                    res.redirect("/campgrounds/" + campground._id);
+                }
+            })
 
+        }
+    })
+
+})
 
 
 app.listen(3000);
---
+++ models/campgrounds.js
@@ -0,0 +1,36 @@
+var mongoose = require("mongoose");
+
+
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    description: String,
+    imageCity: String,
+    descriptionCity: String,
+    comments: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Comment"
+        }
+    ]
+});
+
+module.exports = mongoose.model("Campground", campgroundSchema);
+
+// save campground to db
+// Campground.create({
+//         name: "City 22",
+//         image: " ",
+//         imageCity: "",
+//         description: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
+//         descriptionCity: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)."
+//     },
+//     function (err, campground) {
+//         if (err) {
+//             console.log("some error was made when creating a new campground");
+//             console.log(err);
+//         } else {
+//             console.log("campground has benn saved to db");
+//             console.log(campground);
+//         }
+//     })
